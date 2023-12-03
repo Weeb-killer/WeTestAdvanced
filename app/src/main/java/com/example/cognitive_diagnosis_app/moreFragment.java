93cdb40f1019070c5c341ea216a4a8a33c838686
Weeb-killer <1601255547@qq.com>
2023-12-03 17:09:32 +0800

    老师端新增加入题目功能
    没改初始化数据的语句。这个功能加入题目的时候知识点用","隔开

--- app/src/main/java/com/example/cognitive_diagnosis_app/moreFragment.java
+++ app/src/main/java/com/example/cognitive_diagnosis_app/moreFragment.java
@@ -1,5 +1,7 @@
 package com.example.cognitive_diagnosis_app;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.gson.internal.bind.SqlDateTypeAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +29,8 @@ public class moreFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    SQLiteDatabase mydb;
 
     public moreFragment() {
         // Required empty public constructor
@@ -59,12 +67,52 @@ public class moreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false);
+        View view = inflater.inflate(R.layout.fragment_more, container, false);
+        Button upQ = view.findViewById(R.id.uploadquestion);
+        upQ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View moreF = getView();
+                mydb = SQLiteDatabase.openDatabase("/data/data/com.example.cognitive_diagnosis_app/databases/student.db", null, SQLiteDatabase.OPEN_READWRITE);
+                ContentValues values = new ContentValues();
+                EditText QCo,QA,QB,QC,QD,QRA,QE,KP1;
+                QCo=moreF.findViewById(R.id.Qcontent);
+                QA=moreF.findViewById(R.id.QA);
+                QB=moreF.findViewById(R.id.QB);
+                QC=moreF.findViewById(R.id.QC);
+                QD=moreF.findViewById(R.id.QD);
+                QRA=moreF.findViewById(R.id.QRA);
+                QE=moreF.findViewById(R.id.QE);
+                KP1=moreF.findViewById(R.id.KP);
+                values.put("content", QCo.getText().toString());
+                values.put("option_A", QA.getText().toString());
+                values.put("option_B", QB.getText().toString());
+                values.put("option_C", QC.getText().toString());
+                values.put("option_D", QD.getText().toString());
+                values.put("correct_ot", QRA.getText().toString());
+                values.put("explanation", QE.getText().toString());
+                String str = KP1.getText().toString();
+                System.out.println(str);
+                String[] KP = str.split(",");
+                values.put("Search_Algorithm", KP[0]);
+                values.put("sorting_algorithm", KP[1]);
+                values.put("Link_List", KP[2]);
+                values.put("queue", KP[3]);
+                values.put("stack", KP[4]);
+                values.put("array", KP[5]);
+                values.put("tree", KP[6]);
+                values.put("graph", KP[7]);
+                mydb.insert("timutimu", null, values);
+            }
+        });
+        return view;
     }
+
 
     @Override
     public void onViewCreated(View view, Bundle saveInstanceState) {
         super.onViewCreated(view, saveInstanceState);
-        View fragmentView = getView();
+
     }
+
 }
